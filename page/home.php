a7e2c6b6b0d47048cc6ca70d23ed163ce13a2332
Sukma Anggarmadi <2111527001_sukma@student.unand.ac.id>
2023-05-17 08:21:14 +0700

    ganti font

--- page/home.php
+++ page/home.php
@@ -5,8 +5,14 @@
 	Penentuan Penerima Berprestasi <br>
 	Fakultas Teknologi Informasi <br>
 	Universitas Andalas <br>
-	Menggunakan Metode Simple Additive Weighting (SAW)
+	Menggunakan Metode Simple Additive Weighting (SAW)<br>
 </h1> 
+<h3 class= "denfont">Oleh :<br>
+	1. Muhammad Irsyadul Fikri<br>
+	2. Thomas Nobel Asfar<br>
+	3. Ghina Fitri Hidayah<br>
+	4. Hasya Zikra Alfrena<br>
+	5. Sukma Anggarmadi</h3>
 </b>
 </p>
 </font>
